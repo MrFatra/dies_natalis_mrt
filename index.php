53bd5f7f9033ac80bef4d9c5bdedb765d1c7a111
taufan12345 <abaysurbakti17@gmail.com>
2024-12-08 23:19:42 +0700

    Fix Footer

--- index.php
+++ index.php
@@ -45,6 +45,7 @@
 <body>
     <!-- pake php include per-section -->
     <?php include 'ilmuwan.html'; ?>
+    <?php include 'componentFooter.html'; ?>
 
     <!-- cara pemakaian aos -->
     <div data-aos="fade-in" class="bg-primary w-20 h-20"></div>
